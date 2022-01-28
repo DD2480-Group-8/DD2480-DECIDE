748e8c5e778ca2125fce8e617686ba334bfb8499
Simon <sosnes@kth.se>
2022-01-28 14:59:51 +0100

    implement LIC3

--- src/Decide.java
+++ src/Decide.java
@@ -25,11 +25,41 @@ public class Decide {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+//        System.out.println("Hello World");
     }
 
     public int simpleAddition(int x, int y) {
         return x + y;
+    }
+
+        /*
+    *   LIC 3 is:
+    *   There exists at least one set of three consecutive data points that are the vertices of a triangle
+    *   with area greater than AREA1. (0 ≤ AREA1)
+    *
+    */
+    private boolean LIC3(){
+        if (NUMPOINTS < 3) {
+            return false;
+        }
+
+        for (int n = 0 ; n < NUMPOINTS-2 ; n++) {
+            // Extract the coordinates of the three consecutive data points
+            double x1 = POINTS[n].XPOS;
+            double y1 = POINTS[n].YPOS;
+            double x2 = POINTS[n+1].XPOS;
+            double y2 = POINTS[n+1].YPOS;
+            double x3 = POINTS[n+2].XPOS;
+            double y3 = POINTS[n+2].YPOS;
+
+            // Calculate the area of the three consecutive data points
+            double area = Math.abs((x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))/2);
+
+            if (area > PARAMETERS.AREA1) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }   
