2fcbda9420427d2681d77c2a9db64331f65b1c9f
Emil Myresten <emil.myresten@gmail.com>
2022-02-01 13:58:21 +0100

    Top level documentation added to Coordinate class. #2

--- src/main/java/Coordinate.java
+++ src/main/java/Coordinate.java
@@ -1,3 +1,8 @@
+/**
+ * Coordinate class
+ * Custom type used to model the coordinates of radar data used in the Decide problem.
+ * The properties of the coordinate class are used when calculating the LICs inside the Decide class.
+ */
 public class Coordinate {
     double XPOS;
     double YPOS;
